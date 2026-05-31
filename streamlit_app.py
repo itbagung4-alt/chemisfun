7e5f865f3724e0ee301c11bcc979e7a3b6c4ba9e
itbagung4-alt <itbagung4@gmail.com>
2026-06-01 00:16:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,37 @@ from chempy import balance_stoichiometry
 
 # --- 1. DATABASE ION DASAR ---
 # Format: 'Simbol': (Muatan, Apakah Poliatomik?)
+# --- 1. DATABASE ION SUPER LENGKAP ---
+# Format: 'Simbol': (Muatan, Apakah Poliatomik?)
+
 kation_db = {
-    'Ag': (1, False), 'Na': (1, False), 'K': (1, False), 'H': (1, False), 'Li': (1, False),
-    'Ba': (2, False), 'Ca': (2, False), 'Mg': (2, False), 'Cu': (2, False), 'Pb': (2, False), 'Zn': (2, False),
-    'Al': (3, False), 'Fe': (3, False)
+    # Golongan 1 (Alkali) & Hidrogen (+1)
+    'H': (1, False), 'Li': (1, False), 'Na': (1, False), 'K': (1, False), 'Rb': (1, False), 'Cs': (1, False),
+    # Golongan 2 (Alkali Tanah) (+2)
+    'Be': (2, False), 'Mg': (2, False), 'Ca': (2, False), 'Sr': (2, False), 'Ba': (2, False),
+    # Logam Transisi & Utama Lainnya (Menggunakan Valensi Paling Umum/Stabil)
+    'Ag': (1, False), 'Zn': (2, False), 'Cd': (2, False), 'Al': (3, False), 'Bi': (3, False),
+    'Cu': (2, False), 'Fe': (3, False), 'Pb': (2, False), 'Ni': (2, False), 'Co': (2, False), 
+    'Mn': (2, False), 'Cr': (3, False), 'Sn': (2, False), 'Hg': (2, False),
+    # Kation Poliatomik
+    'NH4': (1, True)
 }
 
 anion_db = {
-    'Cl': (-1, False), 'Br': (-1, False), 'I': (-1, False), 'F': (-1, False),
-    'NO3': (-1, True), 'OH': (-1, True), 'CH3COO': (-1, True),
-    'SO4': (-2, True), 'CO3': (-2, True), 'CrO4': (-2, True), 'S': (-2, False),
-    'PO4': (-3, True)
+    # Halogen & Anion Tunggal (-1)
+    'F': (-1, False), 'Cl': (-1, False), 'Br': (-1, False), 'I': (-1, False),
+    # Anion Poliatomik (-1)
+    'OH': (-1, True), 'NO3': (-1, True), 'NO2': (-1, True), 'CN': (-1, True), 'SCN': (-1, True), 
+    'CH3COO': (-1, True), 'ClO': (-1, True), 'ClO2': (-1, True), 'ClO3': (-1, True), 'ClO4': (-1, True), 
+    'MnO4': (-1, True), 'HCO3': (-1, True), 'HSO4': (-1, True), 'H2PO4': (-1, True),
+    # Anion Tunggal (-2)
+    'O': (-2, False), 'S': (-2, False),
+    # Anion Poliatomik (-2)
+    'SO4': (-2, True), 'SO3': (-2, True), 'CO3': (-2, True), 'CrO4': (-2, True), 'Cr2O7': (-2, True), 
+    'C2O4': (-2, True), 'S2O3': (-2, True), 'HPO4': (-2, True),
+    # Anion Poliatomik & Tunggal (-3)
+    'PO4': (-3, True), 'PO3': (-3, True), 'AsO4': (-3, True), 'N': (-3, False), 'P': (-3, False)
+}
 }
 
 # --- 2. FUNGSI LOGIKA KIMIA ---
